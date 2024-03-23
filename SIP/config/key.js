96f093de483afd14a3bcffaa4e06addfec46f409
ashish goswami <ashishgoswami6298@gmail.com>
2024-03-23 13:05:25 +0530

    done

---
+++ SIP/config/key.js
@@ -0,0 +1,7 @@
+module.exports = {
+  google: {
+    clientID: "782745095508-co4tvm6fi8iihri23tqc9l73dv523ih2.apps.googleusercontent.com",
+    clientSecret: "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",
+  },
+  mongoURI: "mongodb+srv://ashish:goswami@cluster0.trds1g8.mongodb.net/test",
+};
